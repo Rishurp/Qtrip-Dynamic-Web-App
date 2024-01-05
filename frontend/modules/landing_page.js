66d22adbc54fa161d00f038abc0c912a9323425d
RpRishu <rishu.pandey@criodo.com>
2024-01-05 15:51:57 +0530

    “ME_QTRIPDYNAMIC_MODULE_CITIES”  (Crio.Do)

--- frontend/modules/landing_page.js
+++ frontend/modules/landing_page.js
@@ -1,9 +1,10 @@
 import config from "../conf/index.js";
 
+
 async function init() {
   //Fetches list of all cities along with their images and description
   let cities = await fetchCities();
-
+  
   //Updates the DOM with the cities
   if (cities) {
     cities.forEach((key) => {
@@ -16,13 +17,34 @@ async function init() {
 async function fetchCities() {
   // TODO: MODULE_CITIES
   // 1. Fetch cities using the Backend API and return the data
-
+  try{
+    let res = await fetch(config.backendEndpoint +"/cities")
+    let data = await res.json();
+    return data;
+  }
+  catch(err){
+      return null;
+  }
 }
 
 //Implementation of DOM manipulation to add cities
 function addCityToDOM(id, city, description, image) {
   // TODO: MODULE_CITIES
   // 1. Populate the City details and insert those details into the DOM
+
+  let card = document.createElement("a");
+  let data = document.querySelector( "#data" );
+  card.setAttribute("href",`pages/adventures/?city=${id}`)
+  card.setAttribute("id",`${id}`);
+  data.setAttribute("class", " row row-cols-1 row-cols-sm-2 row-cols-xl-4");
+  card.setAttribute("class", "tile mb-3");
+  card.innerHTML = `
+  <img src=${image} ></img>
+  <div class="tile-text">
+  <h5>${city}</h5>
+  <p>${description}</p>
+  `
+  data.append(card);
 
 }
 
