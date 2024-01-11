a850d448f9609b135c4ba4df98a78bf574ab4472
RpRishu <rishu.pandey@criodo.com>
2024-01-11 15:58:03 +0530

    ME_QTRIPDYNAMIC_MODULE_ADVENTURE_DETAILS  (Crio.Do)

--- frontend/conf/index.js
+++ frontend/conf/index.js
@@ -1,3 +1,3 @@
 
-const config = { backendEndpoint: "http://3.110.200.217:8082" };
+const config = { backendEndpoint: "http://3.110.232.177:8082" };
 export default config;
--- frontend/modules/adventure_details_page.js
+++ frontend/modules/adventure_details_page.js
@@ -5,6 +5,9 @@ function getAdventureIdFromURL(search) {
   // TODO: MODULE_ADVENTURE_DETAILS
   // 1. Get the Adventure Id from the URL
 
+  let urlParams = new URLSearchParams(search);
+  let adventureID = urlParams.get("adventure");
+  return adventureID;
 
   // Place holder for functionality to work in the Stubs
   return null;
@@ -13,10 +16,17 @@ function getAdventureIdFromURL(search) {
 async function fetchAdventureDetails(adventureId) {
   // TODO: MODULE_ADVENTURE_DETAILS
   // 1. Fetch the details of the adventure by making an API call
-
-
+  try{
+    let res = await fetch(config.backendEndpoint + `/adventures/detail?adventure=${adventureId}`);
+    let data = await res.json();
+    return data;
+  }
+  catch(err)
+  {
+    return null;
+  }
   // Place holder for functionality to work in the Stubs
-  return null;
+
 }
 
 //Implementation of DOM manipulation to add adventure details to DOM
@@ -24,13 +34,67 @@ function addAdventureDetailsToDOM(adventure) {
   // TODO: MODULE_ADVENTURE_DETAILS
   // 1. Add the details of the adventure to the HTML DOM
 
+  let adventName = document.getElementById("adventure-name");
+  let adventSubtitle = document.getElementById("adventure-subtitle");
+  let photoGallery = document.getElementById("photo-gallery");
+  let adventContent = document.getElementById("adventure-content");
+
+    adventName.textContent = adventure.name;
+    adventSubtitle.textContent = adventure.subtitle;
+    adventure.images.forEach((img) =>{
+    photoGallery.innerHTML += `
+    <div ><img src=${img} class="activity-card-image"/></div>`
+    });
+    adventContent.textContent = adventure.content;
 }
 
 //Implementation of bootstrap gallery component
 function addBootstrapPhotoGallery(images) {
   // TODO: MODULE_ADVENTURE_DETAILS
   // 1. Add the bootstrap carousel to show the Adventure images
+   let photoGallery = document.getElementById("photo-gallery");
+    photoGallery.innerHTML = "";
+    photoGallery.innerHTML =`
+    
+    <div id="carouselIndicators" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators" id="btn-div">
+  </div>
+  <div class="carousel-inner" id="images-div"></div>
+  
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+    
+    `
+  let slideBtn = document.getElementById("btn-div");
+  let imageDiv = document.getElementById("images-div");
 
+  images.forEach((img,index) =>{
+
+    const btnClass = index ==0 ?"active":"";
+    const  ariaCurrent = index == 0 ? "true":"false";
+    const btn = `<button type="button" data-bs-target="#carouselIndicators" data-bs-slide-to=${index} class=${btnClass} aria-current=${ariaCurrent} aria-label="Slide ${index +1}"></button>`
+
+    const imageClass = index == 0 ? "carousel-item active": "carousel-item";
+    
+    const imgSubDivs = `<div class="${imageClass}">
+    <img src=${img} class="d-block activity-card-image " alt="Slide ${index+1}">
+  </div>`
+
+    slideBtn.innerHTML +=btn;;
+    imageDiv.innerHTML +=imgSubDivs;
+
+
+  });
+
+
+  
 }
 
 //Implementation of conditional rendering of DOM based on availability
